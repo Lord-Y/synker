0351428b4f18194e5167a770f3d525a592c18844
Lord-Y <Lord-Y@users.noreply.github.com>
2021-11-09 10:33:50 +0100

    [cmd] - Update vars

--- cmd/vars.go
+++ cmd/vars.go
@@ -2,17 +2,15 @@ package cmd
 
 import (
 	"github.com/Lord-Y/synker/api"
-	"github.com/Lord-Y/synker/models"
 	"github.com/Lord-Y/synker/validate"
 )
 
 // List of vars that will be used by Synker
 var (
-	revision     string
-	buildDate    string
-	goVersion    string
-	Version      = "0.0.1-dev"
-	configStruct models.Configuration
-	cmdValidate  validate.Validate
-	cmdAPI       api.API
+	revision    string
+	buildDate   string
+	goVersion   string
+	Version     = "0.0.1-dev"
+	cmdValidate validate.Validate
+	cmdAPI      api.API
 )
