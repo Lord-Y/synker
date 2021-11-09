66dc46692fe7ce677d62ae9f8e6f5e037ae46036
Lord-Y <Lord-Y@users.noreply.github.com>
2021-11-09 10:37:47 +0100

    [commons] - Add etcd common functions

--- commons/commons.go
+++ commons/commons.go
@@ -15,3 +15,33 @@ func BuildDSN() string {
 func GetEleasticsearchURI() string {
 	return strings.TrimSpace(os.Getenv("SKR_ELASTICSEARCH_URI"))
 }
+
+// GetEtcdURI permit to retrieve OS env variable
+func GetEtcdURI() string {
+	return strings.TrimSpace(os.Getenv("SKR_ETCD_URI"))
+}
+
+// GetEtcdUser permit to retrieve OS env variable
+func GetEtcdUser() string {
+	return strings.TrimSpace(os.Getenv("SKR_ETCD_USER"))
+}
+
+// GetEtcdPassword permit to retrieve OS env variable
+func GetEtcdPassword() string {
+	return strings.TrimSpace(os.Getenv("SKR_ETCD_PASSWORD"))
+}
+
+// GetEtcdCACert permit to retrieve OS env variable
+func GetEtcdCACert() string {
+	return strings.TrimSpace(os.Getenv("SKR_ETCD_CACERT"))
+}
+
+// GetEtcdCert permit to retrieve OS env variable
+func GetEtcdCert() string {
+	return strings.TrimSpace(os.Getenv("SKR_ETCD_CERT"))
+}
+
+// GetEtcdKey permit to retrieve OS env variable
+func GetEtcdKey() string {
+	return strings.TrimSpace(os.Getenv("SKR_ETCD_KEY"))
+}
