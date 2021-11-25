2cce1ade986d05976b7ad6f84ff979641d182d3a
Lord-Y <Lord-Y@users.noreply.github.com>
2021-11-25 23:26:04 +0100

    [docker|cockroach] - Add resolved option

--- docker/cockroach_init.sh
+++ docker/cockroach_init.sh
@@ -10,4 +10,4 @@ cockroach sql --insecure --host ${COCKROACH_HOST} --execute "SHOW CLUSTER SETTIN
 
 cockroach sql --insecure --host ${COCKROACH_HOST} --execute "SHOW CHANGEFEED JOBS" --format tsv | \
   grep kafka || \
-  cockroach sql --insecure --host ${COCKROACH_HOST} --execute "CREATE CHANGEFEED FOR TABLE promo_codes, rides, user_promo_codes, users, vehicle_location_histories, vehicles INTO 'kafka://localhost:9092' WITH full_table_name, on_error = 'pause', protect_data_from_gc_on_pause, updated" --database movr
+  cockroach sql --insecure --host ${COCKROACH_HOST} --execute "CREATE CHANGEFEED FOR TABLE promo_codes, rides, user_promo_codes, users, vehicle_location_histories, vehicles INTO 'kafka://localhost:9092' WITH full_table_name, on_error = 'pause', protect_data_from_gc_on_pause, updated, resolved" --database movr
