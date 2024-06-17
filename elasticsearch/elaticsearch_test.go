b962e223eecb852148ff444c7675bb9240f1d7ab
Lord-Y <Lord-Y@users.noreply.github.com>
2024-06-17 20:22:00 +0200

    [elasticsearch] - Remove old unit tests

--- elasticsearch/elaticsearch_test.go
+++ elasticsearch/elaticsearch_test.go
@@ -2,7 +2,6 @@
 package elasticsearch
 
 import (
-	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -16,17 +15,6 @@ func TestPing(t *testing.T) {
 
 func TestPing_fail_auth(t *testing.T) {
 	assert := assert.New(t)
-	b := Ping()
-	assert.Equal(true, b)
-}
-
-func TestPing_fail(t *testing.T) {
-	assert := assert.New(t)
-
-	os.Setenv("SYNKER_ELASTICSEARCH_USER", "youhou")
-	os.Setenv("SYNKER_ELASTICSEARCH_PASSWORD", "youhou")
-	defer os.Unsetenv("SYNKER_ELASTICSEARCH_USER")
-	defer os.Unsetenv("SYNKER_ELASTICSEARCH_PASSWORD")
 	b := Ping()
 	assert.Equal(true, b)
 }
@@ -74,55 +62,6 @@ func TestCreateIndex(t *testing.T) {
 		}
 	}
 	`
-
-	client, err := Client()
-	assert.Nil(err)
-
-	b, err := createIndex(client, "twitter", mapping)
-	assert.Nil(err)
-	assert.Equal(true, b)
-}
-
-func TestCreateIndex_fail(t *testing.T) {
-	assert := assert.New(t)
-
-	mapping := `
-	{
-		"settings":{
-			"number_of_shards":1,
-			"number_of_replicas":0
-		},
-		"mappings":{
-			"properties":{
-				"user":{
-					"type":"keyword"
-				},
-				"message":{
-					"type":"text",
-					"store": true,
-					"fielddata": true
-				},
-				"retweets":{
-					"type":"long"
-				},
-				"tags":{
-					"type":"keyword"
-				},
-				"location":{
-					"type":"geo_point"
-				},
-				"suggest_field":{
-					"type":"completion"
-				}
-			}
-		}
-	}
-	`
-
-	os.Setenv("SYNKER_ELASTICSEARCH_USER", "youhou")
-	os.Setenv("SYNKER_ELASTICSEARCH_PASSWORD", "youhou")
-	defer os.Unsetenv("SYNKER_ELASTICSEARCH_USER")
-	defer os.Unsetenv("SYNKER_ELASTICSEARCH_PASSWORD")
 
 	client, err := Client()
 	assert.Nil(err)
