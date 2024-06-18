144d8b3f6f0f0bec3c2df8b629afeac1330e8056
Lord-Y <Lord-Y@users.noreply.github.com>
2024-06-18 07:16:10 +0200

    [cmd/api] - Move synker env required variables into api command

--- api/api.go
+++ api/api.go
@@ -25,22 +25,6 @@ func init() {
 	os.Setenv("SYNKER_BATCH_LOG", "true")
 	defer os.Unsetenv("APP_BATCH_LOG")
 	apiLogger.SetLoggerLogLevel()
-
-	if strings.TrimSpace(os.Getenv("SYNKER_PG_URI")) == "" {
-		msg := "SYNKER_PG_URI environment variable must be set"
-		log.Fatal().Err(fmt.Errorf(msg)).Msg(msg)
-		return
-	}
-	if strings.TrimSpace(os.Getenv("SYNKER_ELASTICSEARCH_URI")) == "" {
-		msg := "SYNKER_ELASTICSEARCH_URI environment variable must be set"
-		log.Fatal().Err(fmt.Errorf(msg)).Msg(msg)
-		return
-	}
-	if strings.TrimSpace(os.Getenv("SYNKER_KAFKA_URI")) == "" {
-		msg := "SYNKER_KAFKA_URI environment variable must be set"
-		log.Fatal().Err(fmt.Errorf(msg)).Msg(msg)
-		return
-	}
 }
 
 // Run will start the api server
--- cmd/api.go
+++ cmd/api.go
@@ -2,6 +2,12 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/rs/zerolog/log"
+
 	"github.com/urfave/cli/v2"
 )
 
@@ -20,6 +26,19 @@ func API(c *cli.Context) (z *cli.Command) {
 			},
 		},
 		Action: func(c *cli.Context) error {
+			if strings.TrimSpace(os.Getenv("SYNKER_PG_URI")) == "" {
+				msg := "SYNKER_PG_URI environment variable must be set"
+				log.Fatal().Err(fmt.Errorf(msg)).Msg(msg)
+			}
+			if strings.TrimSpace(os.Getenv("SYNKER_ELASTICSEARCH_URI")) == "" {
+				msg := "SYNKER_ELASTICSEARCH_URI environment variable must be set"
+				log.Fatal().Err(fmt.Errorf(msg)).Msg(msg)
+			}
+			if strings.TrimSpace(os.Getenv("SYNKER_KAFKA_URI")) == "" {
+				msg := "SYNKER_KAFKA_URI environment variable must be set"
+				log.Fatal().Err(fmt.Errorf(msg)).Msg(msg)
+			}
+
 			cmdValidate.Run()
 			cmdAPI.Run(&cmdValidate)
 			return nil
