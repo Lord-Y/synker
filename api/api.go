51b7818abd3e7e3d221aec9dea93a003068ca5e0
Lord-Y <Lord-Y@users.noreply.github.com>
2024-06-19 17:09:06 +0200

    [api] - Improve http server requirements

--- api/api.go
+++ api/api.go
@@ -29,9 +29,57 @@ func init() {
 
 // Run will start the api server
 func (c *API) Run(validated *processing.Validate) {
-	var srv *http.Server
-	router := routers.SetupRouter()
+	var appPort string
+	port := strings.TrimSpace(os.Getenv("SYNKER_API_PORT"))
 
+	switch strings.HasPrefix(port, ":") {
+	case true:
+		appPort = port
+	case false:
+		if port == "" {
+			appPort = ":8080"
+		} else {
+			appPort = fmt.Sprintf(":%s", port)
+		}
+	}
+
+	router := routers.SetupRouter()
+	srv := &http.Server{
+		Addr:    appPort,
+		Handler: router,
+	}
+	log.Info().Msgf("Starting api server on port %s", appPort)
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatal().Err(err).Msg("Startup api server failed")
+		}
+	}()
+
+	go c.runPrerequisitesAndStartProcessing(validated)
+
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 minutes.
+	quit := make(chan os.Signal, 1)
+	// kill (no param) default send syscanll.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall. SIGKILL but can"t be catch, so don't need add it
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Info().Msg("Shutting down api server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal().Err(err).Msg("API server shutted down abruptly")
+	}
+	log.Info().Msg("API server exited successfully")
+}
+
+// runPrerequisitesAndStartProcessing permit to run all functions
+// related to Kafka, elasticsearch and cockroach feeds
+// and then start processing all kafka messages
+func (c *API) runPrerequisitesAndStartProcessing(validated *processing.Validate) {
 	os.Setenv("SYNKER_CONFIG_DIR", c.ConfigDir)
 	defer os.Unsetenv("SYNKER_CONFIG_DIR")
 
@@ -51,42 +99,5 @@ func (c *API) Run(validated *processing.Validate) {
 		return
 	}
 
-	appPort := strings.TrimSpace(os.Getenv("SYNKER_API_PORT"))
-	if appPort != "" {
-		srv = &http.Server{
-			Addr:    fmt.Sprintf(":%s", appPort),
-			Handler: router,
-		}
-		log.Info().Msgf("Starting api server on port %s", appPort)
-	} else {
-		srv = &http.Server{
-			Addr:    ":8080",
-			Handler: router,
-		}
-		log.Info().Msg("Starting api server on port 8080")
-	}
-
-	go func() {
-		validated.Processing()
-		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatal().Err(err).Msg("Startup api server failed")
-		}
-	}()
-
-	// Wait for interrupt signal to gracefully shutdown the server with
-	// a timeout of 5 seconds.
-	quit := make(chan os.Signal, 1)
-	// kill (no param) default send syscanll.SIGTERM
-	// kill -2 is syscall.SIGINT
-	// kill -9 is syscall. SIGKILL but can"t be catch, so don't need add it
-	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-	<-quit
-	log.Info().Msg("Shutting down api server")
-
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-	if err := srv.Shutdown(ctx); err != nil {
-		log.Fatal().Err(err).Msg("API server shutted down abruptly")
-	}
-	log.Info().Msg("API server exited successfully")
+	validated.Processing()
 }
